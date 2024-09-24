5d094a3b445df84105339a4b67971002ea6a8e17
CreativeDad <oyebolang@gmail.com>
2024-09-24 11:20:28 +0100

    Verifying vercel

--- next.config.mjs
+++ next.config.mjs
@@ -38,6 +38,10 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
 };
 
 export default nextConfig;
